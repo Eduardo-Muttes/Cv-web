41b076d8920081d5218e1bcbfb870649a028ad79
Eduardo <eduardomuttes@gmail.com>
2020-08-26 21:10:17 -0300

    Animated Background

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    color: ['#DA0463', '#404B69', '#DBEDF3'],
+    connectParticles: true
+  });
+};
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
