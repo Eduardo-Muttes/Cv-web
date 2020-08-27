886a6c528208adbc021c5d03c28deca058283666
Eduardo <eduardomuttes@gmail.com>
2020-08-26 21:15:21 -0300

    animated background

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   Particles.init({
     selector: '.background',
+    maxParticles: 200,
     color: ['#DA0463', '#404B69', '#DBEDF3'],
     connectParticles: true
   });
